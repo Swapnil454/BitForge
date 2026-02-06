a037e31f1bdaeb3dd691d8186a07e2512816f0a7
Swapnil Shelke <shivamshelke39@gmail.com>
2026-02-06 15:56:16 +0530

    fixed cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -40,12 +40,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-app.use(cors({
-  origin: "http://localhost:3000", // frontend URL
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
